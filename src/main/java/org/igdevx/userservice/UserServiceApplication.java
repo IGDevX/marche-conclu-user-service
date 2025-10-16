b2e0283e6b119c307b9993bb30d476ce7fc3086f
Alexandre-Roussel48 <alexandre.roussel.48@gmail.com>
2025-10-16 10:25:35 +0200

    Initial commit

---
+++ src/main/java/org/igdevx/userservice/UserServiceApplication.java
@@ -0,0 +1,13 @@
+package org.igdevx.userservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/igdevx/userservice/UserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.igdevx.userservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
